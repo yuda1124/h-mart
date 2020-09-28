b5645c7d1d58ae861d848772fac43e06c3b7ec97
yuda1124 <yuda1124@naver.com>
2020-09-28 14:56:41 +0900

    feat: setup - 기본 서버, typescript
    내용을 기술 (최대한 이유를 작성)
    
    related issue: #issue
    related commit: commit_hash

---
+++ server/app.ts
@@ -0,0 +1,46 @@
+import createError from 'http-errors';
+import express, { Request, Response, NextFunction } from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+
+const app = express();
+
+interface Error {
+  status?: number;
+  message?: string;
+}
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../client/build'));
+app.engine('html', require('ejs').renderFile);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err: Error, req: Request, res: Response, next: NextFunction) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+});
+
+module.exports = app;
