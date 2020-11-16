cfc3935a1fc8b7fdda9b1221e77548c938f7b891
yuda1124 <yuda1124@naver.com>
2020-11-16 17:07:07 +0900

    feat: 변경 - 비밀번호를 해싱한 후 저장하도록 로직
    - todo: 로그인 시 decode 한 뒤 비교하는 로직 구현

--- server/routes/userRouter.ts
+++ server/routes/userRouter.ts
@@ -3,7 +3,6 @@ import { UserController } from '../controllers';
 import { wrapAsync } from '../utils';
 const router = express.Router();
 
-/* GET users listing. */
 router.post('/', wrapAsync(UserController.create));
 
 export default router;
--- server/services/userService.ts
+++ server/services/userService.ts
@@ -1,10 +1,12 @@
 import UserModel from '../models/userModel';
 import { IUser } from '../../interfaces';
+import { generateHash } from '../utils';
 
 const userModel = new UserModel();
 
 const create = async (user: IUser): Promise<any> => {
-  const newUser = await userModel.create(user);
+  const hash = await generateHash(user.password);
+  const newUser = await userModel.create({ ...user, password: hash });
   return newUser;
 };
 
