3df4824c5acfca6bec3bdba9e0ee74f19632a95b
yuda1124 <yuda1124@naver.com>
2020-11-22 16:47:05 +0900

    feat: 작성 - 회원가입 컴포넌트 마크업

---
+++ client/src/components/Signin/Signin.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { Input } from '..';
+import { Button } from '..';
+import './Signin.scss';
+
+const Signin = () => {
+  const [userInfo, setInfo] = useState({
+    email: '',
+    password: '',
+  });
+
+  const changeInfo = (propName: string) => (value: any) => {
+    setInfo({ ...userInfo, [propName]: value });
+  };
+
+  const { email, password } = userInfo;
+
+  return (
+    <div className="signin">
+      <div className="form_signin">
+        <h2>Sign In</h2>
+        <div className="wrap_input">
+          <Input
+            placeholder="Email"
+            type="text"
+            onChange={changeInfo('email')}
+            text={email}
+          />
+          <Input
+            placeholder="Password"
+            type="password"
+            onChange={changeInfo('password')}
+            text={password}
+          />
+        </div>
+        <div className="wrap_signin_button">
+          <Button text="Sign In" type="empty" onClick={() => {}} />
+          <Button text="Sign Up" type="empty" onClick={() => {}} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signin;
