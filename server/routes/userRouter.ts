61817a67582475a67e44b35a47e2d3d1af7c00b4
yuda1124 <yuda1124@naver.com>
2020-11-25 13:44:45 +0900

    feat: 추가 - signup logic에 validator 미들웨어

--- server/routes/userRouter.ts
+++ server/routes/userRouter.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { UserController } from '../controllers';
 import { wrapAsync } from '../utils';
+import { validateCreateUser } from '../middlewares/validator';
 const router = express.Router();
 
-router.post('/signup', wrapAsync(UserController.create));
+router.post('/signup', validateCreateUser, wrapAsync(UserController.create));
 router.post('/signin', wrapAsync(UserController.signIn));
 export default router;
