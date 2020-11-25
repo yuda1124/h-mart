9d7a7b4503f8edf5576ac3d520d4a6c24c039f94
yuda1124 <yuda1124@naver.com>
2020-11-25 13:42:24 +0900

    feat: 추가 - errormessage, httpstatus

---
+++ server/constants/errorMessage.ts
@@ -0,0 +1,16 @@
+export const ERROR_MESSAGE = {
+  COMMON_TYPE: '입력이 올바르지 않습니다.',
+  EMAIL_EMPTY: '이메일을 입력해주세요.',
+  EMAIL_TYPE: '이메일 형식이 올바르지 않습니다.',
+  NAME_EMPTY: '이름을 입력해주세요.',
+  NAME_TYPE: '이름은 2~10자 문자열 입니다.',
+  NICKNAME_EMPTY: '닉네임을 입력해주세요.',
+  PASSWORD_EMPTY: '비밀번호를 입력해주세요.',
+  PASSWORD_TYPE: '비밀번호는 8~20자 영문/숫자 입니다.',
+  PASSWORD_CONFIRM_EMPTY: '비밀번호 확인을 입력해주세요.',
+  PASSWORD_CONFIRM_EQUAL: '비밀번호와 비밀번호 확인이 다릅니다.',
+  LOGIN_FAILED: '아이디 또는 비밀번호가 올바르지 않습니다.',
+  DUPLICATED_EMAIL: '중복된 이메일 입니다.',
+  UNAUTHORIZED_TOKEN_AND_REQUIRE_LOGIN:
+    '토큰 정보가 유효하지 않습니다.\n다시 로그인해주세요.',
+};
