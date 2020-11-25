96cc08c9f4651a851079f10722df663f83b03ca6
yuda1124 <yuda1124@naver.com>
2020-11-25 13:43:28 +0900

    feat: 추가 - validator
    - joi package 설치
    - signup 검증 함수 작성

---
+++ server/middlewares/validator.ts
@@ -0,0 +1,49 @@
+import Joi, { ValidationError } from 'joi';
+import { Request, Response, NextFunction } from 'express';
+import { HTTP_STATUS, ERROR_MESSAGE } from '../constants';
+
+const EMAIL = Joi.string().email().required().messages({
+  'string.pattern.base': ERROR_MESSAGE.EMAIL_TYPE,
+  'string.empty': ERROR_MESSAGE.EMAIL_EMPTY,
+});
+const NAME = Joi.string().min(2).max(10).required().messages({
+  'string.min': ERROR_MESSAGE.NAME_TYPE,
+  'string.max': ERROR_MESSAGE.NAME_TYPE,
+  'string.empty': ERROR_MESSAGE.NAME_EMPTY,
+});
+const PASSWORD = Joi.string()
+  .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+  .required()
+  .messages({
+    'string.pattern.base': ERROR_MESSAGE.PASSWORD_TYPE,
+    'string.empty': ERROR_MESSAGE.PASSWORD_EMPTY,
+  });
+
+const errorHandler = (
+  error: ValidationError | undefined,
+  res: Response,
+  next: NextFunction
+) => {
+  if (error) {
+    const { details } = error;
+    const messages = details.map((error) => error.message);
+    return res.status(HTTP_STATUS.BAD_REQUEST).json({ messages });
+  }
+  next();
+};
+
+export const validateCreateUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const schema = Joi.object()
+    .keys({
+      email: EMAIL,
+      password: PASSWORD,
+      name: NAME,
+    })
+    .options({ abortEarly: false });
+  const { error } = schema.validate(req.body);
+  errorHandler(error, res, next);
+};
