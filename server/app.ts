74e9e4856eb610cd565bc38a949cca1747427cce
yuda1124 <yuda1124@naver.com>
2020-11-27 11:14:12 +0900

    feat: 변경 - urlencoded option
    - 중첩된 객체를 파싱할 수 있게 하기 위해 extended option을 true로 변경

--- server/app.ts
+++ server/app.ts
@@ -24,7 +24,7 @@ app.set('views', path.join(__dirname, '../client/build'));
 app.engine('html', require('ejs').renderFile);
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(passport.initialize());
