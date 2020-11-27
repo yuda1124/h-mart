5801a8dcba8a7152e888695518878802c825c369
yuda1124 <yuda1124@naver.com>
2020-11-27 11:23:19 +0900

    refactor: 분리 - signup container와 component

---
+++ client/src/components/Signup/Signup.tsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from 'react';
+import { Input } from '../../components';
+import { Button } from '../../components';
+import { Checkbox } from '../../components';
+import { validateCreateUser } from '../../validator';
+import './Signup.scss';
+interface SignupProps {
+  requestSignup: (email: string, password: string, name: string) => void;
+}
+const Signup = ({ requestSignup }: SignupProps) => {
+  const [userInfo, setInfo] = useState({
+    name: '',
+    email: '',
+    password: '',
+    passwordCheck: '',
+    agreeTerm: false,
+  } as Record<string, any>);
+  const [inputError, setError] = useState({} as Record<string, any>);
+  const changeInfo = (propName: string) => (value: any) => {
+    setInfo({ ...userInfo, [propName]: value });
+  };
+  const getErrorMessage = (propName: string): string | null => {
+    if (inputError[propName]) {
+      return inputError[propName];
+    }
+    return null;
+  };
+  const applySignUp = () => {
+    // const inputError = validateCreateUser(userInfo);
+    // console.log(userInfo);
+
+    // if (Object.keys(inputError).length > 0) {
+    //   setError(inputError);
+    //   return;
+    // }
+    requestSignup(userInfo.email, userInfo.password, userInfo.name);
+  };
+
+  const { name, email, password, passwordCheck, agreeTerm } = userInfo;
+
+  return (
+    <div className="signup">
+      <div className="form_signup">
+        <h2>Create Account</h2>
+        <div className="wrap_input">
+          <Input
+            placeholder="Name"
+            type="text"
+            onChange={changeInfo('name')}
+            text={name}
+            errorMessage={getErrorMessage('name')}
+          />
+          <Input
+            placeholder="Email"
+            type="text"
+            onChange={changeInfo('email')}
+            text={email}
+            errorMessage={getErrorMessage('email')}
+          />
+          <Input
+            placeholder="Password"
+            type="password"
+            onChange={changeInfo('password')}
+            text={password}
+            errorMessage={getErrorMessage('password')}
+          />
+          <Input
+            placeholder="Password Check"
+            type="password"
+            onChange={changeInfo('passwordCheck')}
+            text={passwordCheck}
+            errorMessage={getErrorMessage('passwordCheck')}
+          />
+        </div>
+        <div className="policy">
+          <Checkbox
+            onChange={changeInfo('agreeTerm')}
+            agree={agreeTerm}
+            errorMessage={getErrorMessage('agreeTerm')}
+          >
+            <span>I agree to the Terms and Privacy Policy.</span>
+          </Checkbox>
+        </div>
+        <div className="wrap_signup_button">
+          <Button text="Sign Up" type="filled" onClick={applySignUp} />
+          <Button text="Sign In" type="filled" onClick={() => {}} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
--- client/src/pages/Signup/Signup.tsx
+++ client/src/pages/Signup/Signup.tsx
@@ -1,66 +1,14 @@
-import React, { useState } from 'react';
-import { Input } from '../../components';
-import { Button } from '../../components';
-import { Checkbox } from '../../components';
-import './Signup.scss';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Signup as SignupComponent } from '../../components';
+import { signupThunk } from '../../modules/auth';
 
-const Signin = () => {
-  const [userInfo, setInfo] = useState({
-    name: '',
-    email: '',
-    password: '',
-    passwordCheck: '',
-    agree: false,
-  });
-
-  const changeInfo = (propName: string) => (value: any) => {
-    setInfo({ ...userInfo, [propName]: value });
+const Signup = () => {
+  const dispatch = useDispatch();
+  const requestSignup = (email: string, password: string, name: string) => {
+    dispatch(signupThunk(email, password, name));
   };
-
-  const { name, email, password, passwordCheck, agree } = userInfo;
-
-  return (
-    <div className="signin">
-      <div className="form_signin">
-        <h2>Create Account</h2>
-        <div className="wrap_input">
-          <Input
-            placeholder="Name"
-            type="text"
-            onChange={changeInfo('name')}
-            text={name}
-          />
-          <Input
-            placeholder="Email"
-            type="text"
-            onChange={changeInfo('email')}
-            text={email}
-          />
-          <Input
-            placeholder="Password"
-            type="password"
-            onChange={changeInfo('password')}
-            text={password}
-          />
-          <Input
-            placeholder="Password Check"
-            type="password"
-            onChange={changeInfo('passwordCheck')}
-            text={passwordCheck}
-          />
-        </div>
-        <div className="policy">
-          <Checkbox onChange={changeInfo('agree')} agree={agree}>
-            <span>I agree to the Terms and Privacy Policy.</span>
-          </Checkbox>
-        </div>
-        <div className="wrap_signin_button">
-          <Button text="Sign Up" type="filled" onClick={() => {}} />
-          <Button text="Sign In" type="filled" onClick={() => {}} />
-        </div>
-      </div>
-    </div>
-  );
+  return <SignupComponent requestSignup={requestSignup} />;
 };
 
-export default Signin;
+export default Signup;
