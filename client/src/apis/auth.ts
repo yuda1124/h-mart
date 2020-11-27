9d5800e3511f56b798600b06fa009e6879bc3662
yuda1124 <yuda1124@naver.com>
2020-11-27 11:23:53 +0900

    feat: 추가 - signup api

--- client/src/apis/auth.ts
+++ client/src/apis/auth.ts
@@ -5,6 +5,15 @@ export const signin = async (
   email: string,
   password: string
 ): Promise<User> => {
-  const response = await api.post('/user/signIn', { email, password });
+  const response = await api.post('/user/signin', { email, password });
+  return response.data;
+};
+
+export const signup = async (
+  email: string,
+  password: string,
+  name: string
+): Promise<User> => {
+  const response = await api.post('/user/signup', { email, name, password });
   return response.data;
 };
